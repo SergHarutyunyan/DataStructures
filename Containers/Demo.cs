d7127d123d2c2ce0b41d419415a4c0807a76798c
SergHarutyunyan <harser09@gmail.com>
2018-04-30 18:59:40 +0400

    Added Quick-Union implementation

--- Containers/Demo.cs
+++ Containers/Demo.cs
@@ -3,6 +3,7 @@ using Containers.Queue;
 using Containers.Stack;
 using Containers.Tree;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ Containers/Union-Find/QuickUnion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Containers.Union_Find
+{
+    public class QuickUnion
+    {
+        private int[] id;
+
+        public QuickUnion(int N)
+        {
+            id = new int[N];
+            for (int i = 0; i < N; i++)
+                id[i] = i;            
+        }
+
+        private int root(int i)
+        {
+            while (i != id[i]) i = id[i];
+            return i;
+        }
+
+        public bool Connected(int p, int q) { return root(p) == root(q); }
+
+        public void Union(int p, int q)
+        {
+            int i = root(p);
+            int j = root(q);
+            id[i] = j;
+        }
+    }
+}
